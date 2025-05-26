a3de6427383bd83834f232a88040084fe748d5f0
QuanggNguyennDuyy <nguyenduyquang03052002@gmail.com>
2025-05-27 06:12:22 +0700

    Update footer.blade.php

--- resources/views/web/layout/footer.blade.php
+++ resources/views/web/layout/footer.blade.php
@@ -60,7 +60,7 @@
         <div class="row">
             <div class="col-md-12 text-center">
                 <br>
-                <div class="copyright">&copy; Blog. Design: <a href="http://html.design">HTML Design</a>.</div>
+                <div class="copyright">&copy; Blog. Design: HTML Design</a>.</div>
             </div>
         </div>
     </div><!-- end container -->
